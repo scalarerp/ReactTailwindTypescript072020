2290027a7f40f1817b900ff4ac22a323ae14f467
scalarjonas <jg@scalar.inf.br>
2020-08-03 14:46:50 -0300

    2020-08-03-14-46-50

--- src/components/content4/index.js
+++ src/components/content4/index.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import CardImageAndOverlayText from '../cards/cardImageAndOverlayText'
-
+import { RiWhatsappLine } from 'react-icons/ri'
 import p_limpatubo from '../../assets/images/p_limpatubo.jpeg'
 import p_dhein from '../../assets/images/p_dhein.jpeg'
 import p_autosul from '../../assets/images/p_autosul.jpeg'
 import p_massoterapia from '../../assets/images/p_massoterapia.jpeg'
-import vamos_conversar from '../../assets/images/vamos_conversar.png'
+
 import rodape from '../../assets/images/rodape.png'
 
 import { Container, ImageDivBackground64 } from '../misc.js'
@@ -68,17 +68,29 @@ export default () => {
           ))}
         </div>
 
-        <div className=" mt-16 flex flex-col justify-center text-center divide-y divide-gray-400 ">
+        <div
+          id="contato"
+          className=" mt-16 flex flex-col justify-center text-center divide-y divide-gray-400 "
+        >
           <h1 className="text-4xl font-panton font-bold uppercase leading-none tracking-tight text-gray-800 lg:text-6xl dark:text-gray-400">
             Vamos Conversar?
           </h1>
-          <p className="mt-6 mb-12 pt-12 text-lg font-bold text-gray-700 dark:text-gray-400">
-            <ImageDivBackground64 imageSrc={vamos_conversar} />
+
+          <p className="shadow-2xl rounded-lg bg-primary-400 mx-20 mt-10 text-white">
+            <h1 className="text-4xl font-sunn mt-5  ">
+              Envie uma mensagem pelo WhatsApp:
+            </h1>
+            <h1 className="inline-flex justify-center items-center text-center">
+              <RiWhatsappLine className="icon w-24 h-24" />
+              <span className="text-5xl font-panton font-bold uppercase">
+                (51) 99996-1716
+              </span>
+            </h1>
           </p>
         </div>
 
         <div className=" mt-16 flex flex-col justify-center text-center divide-y divide-gray-400 ">
-          <p className=" text-lg font-bold text-gray-700 dark:text-gray-400">
+          <p className="text-lg font-bold text-gray-700 dark:text-gray-400">
             <ImageDivBackground64 imageSrc={rodape} />
           </p>
         </div>
